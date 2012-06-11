496b9a99752dcd31ee60d35f2194f4094c145a52
Laura Suárez <asturdesign@gmail.com>
2012-06-11 20:43:04 +0100

    adding modals to dog breeds. at the moment only German Sheperd works. Try to click on the image to see it in action.

--- js/application.js
+++ js/application.js
@@ -1,12 +1,18 @@
-//mc = {
-//  init: function() {
-//		mc.popOver();
-//  },
-//  
-//  popOver: function() {
-        $('#test_button').popover();
-//		});
-//}}
-//
-//mc.init();
-//
+mc = {
+  init: function() {
+	mc.popover();
+	mc.modals();
+  },
+  
+  modals: function() {
+    $('#german_shepherd').click(){
+        $('#myModal').modal();
+    }
+  },
+  
+  popover: function () {
+    $('#test_button').popover();
+  }
+}
+
+mc.init();
